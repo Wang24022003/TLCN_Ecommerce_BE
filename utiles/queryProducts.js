82f5e24f3d573fb094760601ff6428aaf86cf9a8
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-09-24 13:46:51 +0700

    Search

--- utiles/queryProducts.js
+++ utiles/queryProducts.js
@@ -1,69 +1,131 @@
-class queryProducts {
-     products = []
-     query = {}
-     constructor(products,query){
-         this.products = products
-         this.query = query
-     }
+const categoryModel = require('../../models/categoryModel')
+const productModel = require('../../models/productModel')
+const { responseReturn } = require("../../utiles/response")
+const queryProducts = require('../../utiles/queryProducts')
+class homeControllers{
+
+    formateProduct = (products) => {
+        const productArray = [];
+        let i = 0;
+        while (i < products.length ) {
+            let temp = []
+            let j = i
+            while (j < i + 3) {
+                if (products[j]) {
+                    temp.push(products[j])
+                }
+                j++
+            }
+            productArray.push([...temp])
+            i = j
+        }
+        return productArray
+    }
+
+    get_categorys = async(req,res) => {
+        try {
+            const categorys = await categoryModel.find({})
+            responseReturn(res,200, {
+                categorys
+            })
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    // end method 
+
+    get_products = async(req, res) => {
+        try {
+            const products = await productModel.find({}).limit(12).sort({
+                createdAt: -1
+            })
+            const allProduct1 = await productModel.find({}).limit(9).sort({
+                createdAt: -1
+            })
+            const latest_product = this.formateProduct(allProduct1);
+            
+            const allProduct2 = await productModel.find({}).limit(9).sort({
+                rating: -1
+            })
+            const topRated_product = this.formateProduct(allProduct2);
+           
+            const allProduct3 = await productModel.find({}).limit(9).sort({
+                discount: -1
+            })
+            const discount_product = this.formateProduct(allProduct3);
+
+            responseReturn(res, 200,{
+                products,
+                latest_product,
+                topRated_product,
+                discount_product
+            })
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+   // end method 
+
+   price_range_product = async (req, res) => {
+    try {
+        const priceRange = {
+            low: 0,
+            high: 0,
+        }
+        const products = await productModel.find({}).limit(9).sort({
+            createdAt: -1 // 1 for asc -1 is for Desc
+        })
+        const latest_product = this.formateProduct(products);
+        const getForPrice = await productModel.find({}).sort({
+            'price': 1
+        })
+        if (getForPrice.length > 0) {
+            priceRange.high = getForPrice[getForPrice.length - 1].price
+            priceRange.low = getForPrice[0].price
+        }
+        responseReturn(res, 200, {
+            latest_product,
+            priceRange
+        })
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+
+   }
+
+// end method 
+
+query_products = async (req, res) => {
+    const parPage = 12
+    req.query.parPage = parPage
+
+    try {
+        const products = await productModel.find({}).sort({
+            createdAt: -1
+        })
+        const totalProduct = new queryProducts(products, req.query).categoryQuery().ratingQuery().searchQuery().priceQuery().sortByPrice().countProducts();
+
+        const result = new queryProducts(products, req.query).categoryQuery().ratingQuery().priceQuery().searchQuery().sortByPrice().skip().limit().getProducts();
+        
+        responseReturn(res, 200, {
+            products: result,
+            totalProduct,
+            parPage
+        })
+
+        
+    } catch (error) {
+        console.log(error.message)
+    }
  
-     categoryQuery = () => {
-         this.products = this.query.category ? this.products.filter(c => c.category === this.query.category) : this.products
-         return this
-     }
- 
-     ratingQuery = () => {
-         this.products = this.query.rating ? this.products.filter(c => parseInt(this.query.rating) <= c.rating && c.rating < parseInt(this.query.rating) + 1) : this.products
-         return this
-     }
- 
-     priceQuery = () => {
-         this.products = this.products.filter(p => p.price >= this.query.lowPrice & p.price <= this.query.highPrice )
-         return this
-     }
-     sortByPrice = () => {
-         if (this.query.sortPrice) {
-             if (this.query.sortPrice === 'low-to-high') {
-                 this.products = this.products.sort(function (a,b){ return a.price - b.price})
-             } else {
-                 this.products = this.products.sort(function (a,b){ return b.price - a.price})
-             }
-         }
-         return this
-     }
- 
-     skip = () => {
-         let {pageNumber} = this.query
-         const skipPage = (parseInt(pageNumber) - 1) * this.query.parPage
-         let skipProduct = []
- 
-         for (let i = skipPage; i < this.products.length; i++) {
-             skipProduct.push(this.products[i]) 
-         }
-         this.products = skipProduct
-         return this
-     }
- 
-     limit = () => {
-         let temp = []
-         if (this.products.length > this.query.parPage) {
-             for (let i = 0; i < this.query.parPage; i++) {
-                 temp.push(this.products[i]) 
-             } 
-         }else {
-             temp = this.products
-         }
-         this.products = temp 
-         return this
-     }
- 
-     getProducts = () => {
-         return this.products
-     }
- 
-     countProducts = () => {
-         return this.products.length
-     } 
- 
- }
- 
- module.exports = queryProducts
+}
+// end method 
+
+
+
+}
+
+module.exports = new homeControllers()
